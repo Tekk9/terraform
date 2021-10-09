31b79bfedb994e628cf73fc3b4f6097a4c606fbc
“Tekk9” <lssmrtoboh@gmail.com>
2021-10-09 07:44:43 +0100

    Adopt default vpc and rename tf file to Prod

--- prod.tf
+++ prod.tf
@@ -3,7 +3,9 @@ provider "aws"{
   region  = "eu-west-2"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
   bucket = "tf-materdolorosa-2021"
   acl    = "private"
 }
+
+resource "aws_default_vpc" "default"{} 
